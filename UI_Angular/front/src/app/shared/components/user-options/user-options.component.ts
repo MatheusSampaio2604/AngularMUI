a0ee0d7b542f0b5983ec5b6cea9a84bc9600c4be
Samuel Lucas <samuel.lucasmacedos@gmail.com>
2025-05-23 15:02:15 -0300

    v1 da tela de user options

--- UI_Angular/front/src/app/shared/components/user-options/user-options.component.ts
+++ UI_Angular/front/src/app/shared/components/user-options/user-options.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { User } from "../../../core/models/user.model"
+import { AuthService } from "../../../core/auth/auth.service"
+import { empty } from "rxjs";
 
 @Component({
   selector: 'app-user-options',
@@ -7,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './user-options.component.css'
 })
 export class UserOptionsComponent {
+  user : User
+  constructor(private _authService: AuthService) {
+    this.user = this.getData()
+  }
+  getData(): User {
+    var {FirstName,LastName,level,name,roles} = this._authService.userData()
+    this.user = new User;
+    // this.user.firstName == null ? "Not Defined" : FirstName;
+    // this.user.firstName = this.user.firstName === undefined ? "Empty" : FirstName
+    this.user.firstName = FirstName == undefined ? "Not defined" : FirstName
+    this.user.lastName = LastName == undefined ? "Not defined" : LastName
+    this.user.level = level;
+    this.user.name = name;
+    this.user.userGroups = roles;
 
-}
+    return this.user;
+    }
+  }
